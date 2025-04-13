4986ff4b1ba79258293b66644a1df4c836930703
FroGitHub <andrey8102310@gmail.com>
2025-04-13 18:16:10 +0300

    [API][DB] Add Email API and label

---
+++ src/main/java/ma/student/task/management/controller/LabelController.java
@@ -0,0 +1,58 @@
+package ma.student.task.management.controller;
+
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import ma.student.task.management.dto.label.LabelCreateRequestDto;
+import ma.student.task.management.dto.label.LabelDto;
+import ma.student.task.management.service.LabelService;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/labels")
+@RequiredArgsConstructor
+public class LabelController {
+
+    private final LabelService labelService;
+
+    @GetMapping
+    @PreAuthorize("hasRole('USER')")
+    public Page<LabelDto> getLabels(Pageable pageable) {
+        return labelService.getLabels(pageable);
+    }
+
+    @PostMapping
+    @PreAuthorize("hasRole('USER')")
+    @ResponseStatus(HttpStatus.CREATED)
+    public LabelDto createLabel(
+            @RequestBody @Valid LabelCreateRequestDto createRequestDto) {
+        return labelService.createLabels(createRequestDto);
+    }
+
+    @PutMapping("/{id}")
+    @PreAuthorize("hasRole('USER')")
+    public LabelDto updateLabel(
+            @PathVariable Long id,
+            @RequestBody @Valid LabelCreateRequestDto createRequestDto) {
+        return labelService.updateLabel(id, createRequestDto);
+    }
+
+    @DeleteMapping("/{id}")
+    @PreAuthorize("hasRole('USER')")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteLabel(@PathVariable Long id) {
+        labelService.deleteLabel(id);
+    }
+
+}
---
+++ src/main/java/ma/student/task/management/model/Label.java
@@ -0,0 +1,40 @@
+package ma.student.task.management.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.SQLRestriction;
+
+@Entity
+@Table(name = "labels")
+@Getter
+@Setter
+@SQLDelete(sql = "UPDATE labels SET is_deleted = true WHERE id = ?")
+@SQLRestriction("is_deleted = false")
+public class Label {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(nullable = false)
+    private String name;
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    private Color color;
+    @Column(nullable = false, columnDefinition = "TINYINT(1)")
+    private boolean isDeleted = false;
+
+    public enum Color {
+        RED,
+        GREEN,
+        BLUE
+    }
+}
